1f8e74762708b556792a24404d98dd5569243d9e
Julian Stephens <julian@julianstephens.net>
2026-08-17 19:48:06 -0400

    style gui theme

--- cmd/aeon-gui/theme.go
+++ cmd/aeon-gui/theme.go
@@ -1,11 +1,14 @@
 package main
 
-import (
-	"image/color"
-)
+import "image/color"
 
 var (
-	themeBackground = color.RGBA{R: 15, G: 15, B: 18, A: 255}
-	themePanel      = color.RGBA{R: 28, G: 30, B: 35, A: 255}
-	themeAccent     = color.RGBA{R: 90, G: 160, B: 255, A: 255}
+	themeBackground = color.RGBA{R: 12, G: 16, B: 21, A: 255}
+	themePanel      = color.RGBA{R: 20, G: 26, B: 34, A: 255}
+	themePanelAlt   = color.RGBA{R: 25, G: 32, B: 42, A: 255}
+	themeBorder     = color.RGBA{R: 47, G: 58, B: 72, A: 255}
+	themeAccent     = color.RGBA{R: 96, G: 165, B: 250, A: 255}
+	themeAccentDim  = color.RGBA{R: 45, G: 78, B: 110, A: 255}
+	themeText       = color.RGBA{R: 226, G: 232, B: 240, A: 255}
+	themeMuted      = color.RGBA{R: 148, G: 163, B: 184, A: 255}
 )
