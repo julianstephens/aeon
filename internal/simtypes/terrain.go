3a34472c1796127a579c8556f973a71ebc3c215c
Julian Stephens <julian@julianstephens.net>
2026-08-17 18:09:38 -0400

    fix: remove competing carrying capacity field

--- internal/simtypes/terrain.go
+++ internal/simtypes/terrain.go
@@ -82,14 +82,13 @@ func (tt TerrainType) String() string {
 }
 
 type TerrainCell struct {
-	Location         Position
-	Terrain          TerrainType
-	Elevation        float64
-	Moisture         float64
-	Fertility        float64
-	FoodCapacity     float64
-	CarryingCapacity float64
-	Population       float64
+	Location     Position
+	Terrain      TerrainType
+	Elevation    float64
+	Moisture     float64
+	Fertility    float64
+	FoodCapacity float64
+	Population   float64
 }
 
 type TerrainMap struct {
@@ -195,7 +194,6 @@ func (tm *TerrainMap) ApplyFoodCapacity(layer *Layer) {
 			foodCapacity := layer.Get(x, y)
 			cell := tm.GetCell(x, y)
 			cell.FoodCapacity = foodCapacity
-			cell.CarryingCapacity = foodCapacity * MaxPopulationPerCell
 			tm.SetCell(x, y, *cell)
 		}
 	}
